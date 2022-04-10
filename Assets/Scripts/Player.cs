137fbed4b7002418c81c14db2172b2839e0a5481
Keith Jerosky <kjerosky@gmail.com>
2022-04-09 19:56:51 -0700

    Create world tileset and world filled with ocean tiles

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    void Update() {
+        //TODO: Replace this temporary movement code!
+        int horizontalMovement = (Input.GetKey(KeyCode.D) ? 1 : 0) - (Input.GetKey(KeyCode.A) ? 1 : 0);
+        int verticalMovement = (Input.GetKey(KeyCode.W) ? 1 : 0) - (Input.GetKey(KeyCode.S) ? 1 : 0);
+        transform.position += new Vector3(horizontalMovement, verticalMovement, 0) * 3 * Time.deltaTime;
+    }
+}
