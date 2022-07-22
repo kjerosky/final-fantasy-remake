32e8e750c89d3e04fab6946efd724ade606f2884
Keith Jerosky <kjerosky@gmail.com>
2022-07-22 15:56:54 +0100

    Center and resize unit action queue based on queue size

--- Assets/Scripts/UnitActionQueue.cs
+++ Assets/Scripts/UnitActionQueue.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class UnitActionQueue : MonoBehaviour {
 
     [SerializeField] List<UnitActionQueueSlot> slots;
+    [SerializeField] float spacingBetweenSlotCenters;
 
     public void updateContent(List<BattleUnit> units) {
         int disabledSlotsStartingIndex = units.Count;
@@ -12,10 +13,19 @@ public class UnitActionQueue : MonoBehaviour {
         for (int i = 0; i < disabledSlotsStartingIndex; i++) {
             slots[i].gameObject.SetActive(true);
             slots[i].updateContent(units[i]);
+
+            RectTransform slotRectTransform = slots[i].GetComponent<RectTransform>();
+            Vector2 slotAnchoredPosition = slotRectTransform.anchoredPosition;
+            float newAnchoredPositionX = calculcateAnchoredPositionX(i, units.Count, spacingBetweenSlotCenters);
+            slotRectTransform.anchoredPosition = new Vector2(newAnchoredPositionX, slotAnchoredPosition.y);
         }
 
         for (int i = disabledSlotsStartingIndex; i < slots.Count; i++) {
             slots[i].gameObject.SetActive(false);
         }
+    }
+
+    private float calculcateAnchoredPositionX(int unitIndex, int totalUnits, float spacing) {
+        return -(spacing * (totalUnits - 1) / 2) + (unitIndex * spacing);
     }
 }
