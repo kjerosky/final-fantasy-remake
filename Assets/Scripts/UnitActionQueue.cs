69026884155daae212a2c2b6517f716605ad8690
Keith Jerosky <kjerosky@gmail.com>
2022-07-24 16:12:21 +0100

    Move unit action queue to the right side of screen

--- Assets/Scripts/UnitActionQueue.cs
+++ Assets/Scripts/UnitActionQueue.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class UnitActionQueue : MonoBehaviour {
 
     [SerializeField] List<UnitActionQueueSlot> slots;
-    [SerializeField] float spacingBetweenSlotCenters;
 
     public void updateContent(List<BattleUnit> units) {
         int disabledSlotsStartingIndex = units.Count;
@@ -13,11 +12,6 @@ public class UnitActionQueue : MonoBehaviour {
         for (int i = 0; i < disabledSlotsStartingIndex; i++) {
             slots[i].gameObject.SetActive(true);
             slots[i].updateContent(units[i]);
-
-            RectTransform slotRectTransform = slots[i].GetComponent<RectTransform>();
-            Vector2 slotAnchoredPosition = slotRectTransform.anchoredPosition;
-            float newAnchoredPositionX = calculcateAnchoredPositionX(i, units.Count, spacingBetweenSlotCenters);
-            slotRectTransform.anchoredPosition = new Vector2(newAnchoredPositionX, slotAnchoredPosition.y);
         }
 
         for (int i = disabledSlotsStartingIndex; i < slots.Count; i++) {
@@ -31,9 +25,5 @@ public class UnitActionQueue : MonoBehaviour {
                 slots[i].showSelectionArrow(unitIndices.Contains(i));
             }
         }
-    }
-
-    private float calculcateAnchoredPositionX(int unitIndex, int totalUnits, float spacing) {
-        return -(spacing * (totalUnits - 1) / 2) + (unitIndex * spacing);
     }
 }
