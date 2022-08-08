99adad1329dc4f98c44e73b0c36d30cb0d330329
Keith Jerosky <kjerosky@gmail.com>
2022-08-07 17:50:40 -0700

    Implement NPC that starts a battle after talking to it

---
+++ Assets/Scripts/TalkingBattleNpcController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkingBattleNpcController : MonoBehaviour, Interactable {
+
+    [SerializeField] Dialog dialog;
+
+    private CharacterAnimator animator;
+    private DialogManager dialogManager;
+
+    void Start() {
+        animator = GetComponent<CharacterAnimator>();
+        animator.Horizontal = 0;
+        animator.Vertical = -1;
+        animator.IsMoving = true;
+    }
+
+    public bool isCurrentlyInteractable() {
+        return true;
+    }
+
+    public void interact(Transform initiator) {
+        if (dialogManager == null) {
+            dialogManager = FindObjectOfType<DialogManager>();
+        }
+
+        Vector3 lookDirection = determineLookDirection(initiator.position);
+        animator.Horizontal = lookDirection.x;
+        animator.Vertical = lookDirection.y;
+        animator.IsMoving = false;
+
+        StartCoroutine(dialogManager.showDialog(dialog, () => {
+            //TODO SETUP BATTLE TRANSFER OBJECT HERE BEFORE TRANSITIONING INTO BATTLE
+
+            FindObjectOfType<BattleTransitionManager>().transitionIntoBattle();
+        }));
+    }
+
+    private Vector3 determineLookDirection(Vector3 initiatorPosition) {
+        return (initiatorPosition - transform.position).normalized;
+    }
+}
